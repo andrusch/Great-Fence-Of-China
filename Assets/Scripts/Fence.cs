5e282f8921e63cd8c30e973d3288467c178e9354
Timothy S Andrus <timothy.s.andrus@gmail.com>
2014-10-04 12:31:46 -0500

    fence starting to change image

--- Assets/Scripts/Fence.cs
+++ Assets/Scripts/Fence.cs
@@ -8,16 +8,18 @@ public class Fence : MonoBehaviour {
 	public int y;
 	public int HealthIncrease;
 	public int ClickBonus;
-
+	public int OverlayCount;
+	public int Chunk;
+	public int MaxHealth;
 	// Use this for initialization
 	void Start () {
-		this.Health = 10;
+		this.Health = 0;
 		this.Shielding = 0;
 		this.HealthIncrease = 1;
 		this.ClickBonus = 1;
-
-	
-	
+		this.OverlayCount = 3;
+		this.Chunk = this.Health / (OverlayCount + 1);
+		this.MaxHealth = 100;
 	}
 	
 	// Update is called once per frame
@@ -28,20 +30,42 @@ public class Fence : MonoBehaviour {
 	{
 		if (damage > Shielding) {
 
-			this.Health -= damage;
+			this.Health -= damage + Shielding;
+			 
 		} 
 
 	}
+	public void UpdateImage()
+	{
+		if (Health <= MaxHealth * 1)
+		{
+			//set base fence image
+		}
+		else if (Health <= MaxHealth * 2)
+		{
+			//set broken1 fence image
+		}
+		else if (Health <= MaxHealth * 3)
+		{
+			//set broken2 fence image
+		}
+		else if (Health <= MaxHealth * 4)
+		{
+			//set broken3 fence image
+		}
+	}
 	public void Explode()
 	{
+
 	}
 	public void AddHealth(bool addBonus)
 	{
 				this.Health += this.HealthIncrease;
 				if (addBonus){
 						this.Health += this.ClickBonus;
-		}
 
+		}
+		UpdateImage ();
 	}
 
 }
