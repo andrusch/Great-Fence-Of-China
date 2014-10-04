bff75308dec7a7a218ad9076e74cb1ced7cf0614
Timothy S Andrus <timothy.s.andrus@gmail.com>
2014-10-04 00:03:42 -0500

    fence variables

--- Assets/Scripts/Fence.cs
+++ Assets/Scripts/Fence.cs
@@ -2,14 +2,39 @@
 using System.Collections;
 
 public class Fence : MonoBehaviour {
+	public int Health;
+	public int Shielding;
+	public int x;
+	public int y;
+	public int HealthIncrease;
 
 	// Use this for initialization
 	void Start () {
+		this.Health = 10;
+		this.Shielding = 0;
+		this.HealthIncrease = 1;
+
+	
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	public void TakeDamage(int damage)
+	{
+		if (damage > Shielding) {
+
+			this.Health -= damage;
+		} 
+
+	}
+	public void Explode()
+	{
+	}
+	public void AddHealth()
+	{
+		this.Health += this.HealthIncrease;
 	}
 }
