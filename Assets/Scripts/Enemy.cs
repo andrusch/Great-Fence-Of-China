0d63b8e0735b6c212f5477cce7a902a7041d5b81
Timothy S Andrus <timothy.s.andrus@gmail.com>
2014-10-03 23:27:37 -0500

    no message

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+	public int Power;
+	public int Speed;
+	public bool DoesStun;
+	public int x
+	public int y
 	// Use this for initialization
 	void Start () {
-	
+		this.Power = 1;
+		this.DoesStun = false;
+		this.Speed = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+	public void Move()
+	{
+
+	}
+
+	public void Explode()
+	{
+
+	}
+
+	
+
 }
