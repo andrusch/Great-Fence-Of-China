a983050cc76a8d6d8601b5d3f0716b2a364cb5f8
Timothy S Andrus <timothy.s.andrus@gmail.com>
2014-10-04 00:30:15 -0500

    First player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,14 +2,27 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	public int x;
+	public int y;
+	public int Health;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.Health = 5;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+	public void Move()
+	{
+
+	}
+	public void TakeDamage()
+	{
+
+		}
+
 }
