b5090daacb9748b99acc63c2c45adfc1fdeb2738
Charles H Andrus <charles.andrus@orangeinfusion.com>
2014-10-04 16:49:38 -0500

    Keyboard working.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Player : Piece {
@@ -12,7 +13,8 @@ public class Player : Piece {
 	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
 	private bool m_isHoriAxisInUse = false;
 	private bool m_isVertAxisInUse = false;
-
+    public float XMovementOffset;
+    public float YMovementOffset;
 	// Use this for initialization
 	void Start () {
 		this.Health = 5;
@@ -26,50 +28,30 @@ public class Player : Piece {
 	
 	void FixedUpdate() 
 	{
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
-		float tempX = this.X;
-		float tempY = this.Y;
-		if( h != 0.0f)
-		{
-			if(m_isHoriAxisInUse == false)
-			{
-				if  (((tempX+h) < maxXAndY.x) && ((tempX+h) > minXAndY.x))
-					tempX += h;
-				m_isHoriAxisInUse = true;
-			}
-		}
-		if( h == 0.0f)
-		{
-			m_isHoriAxisInUse = false;
-		} 
-		if( v != 0.0f)
-		{
-			if(m_isVertAxisInUse == false)
-			{
-				if  (((tempY+v) < maxXAndY.y) && ((tempY+v) > minXAndY.y))
-					tempY += v;
-				m_isVertAxisInUse = true;
-			}
-		}
-		if( v == 0.0f)
-		{
-			m_isVertAxisInUse = false;
-		} 
-		Vector2 tempVector = new Vector2(tempX,tempY);
-		//Debug.LogError("testing...");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        float tempX = 0;
+        float tempY = 0;
+        if (v != 0)
+        {
+            tempY += v;
+            tempX += v;
+        }
+        if (tempX != this.X || tempY != this.Y)
+        {
+            if (this.TryMove((int)tempX, (int)tempY))
+            {
+                transform.Translate((float)(tempX * XMovementOffset), (float)(tempY * YMovementOffset), 0);
+            }
+        }
+
 		
-		if (tempX != this.X || tempY != this.Y)
-		{
-			if (this.TryMove((int)tempX, (int)tempY))
-			{
-				gameObject.transform.position = tempVector;
-			}
-		}
 	}
 	
 	public bool TryMove(int newX, int newY)
 	{
+		this.X = newX;
+		this.Y = newY;
 			return true;
 	}
 	
