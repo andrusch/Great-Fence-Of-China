b712edcab49f0523aa6848214870d364bed42996
Charles H Andrus <charles.h.andrus@gmail.com>
2014-10-03 23:32:39 -0500

    Game Engine Code.

---
+++ Assets/Scripts/GameEngine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameEngine : MonoBehaviour {
+	private Object[,] _grid;
+	private List<Fence> _fencePieces;
+	private List<Enemy> _enemies;
+	private Player _player;
+
+	public static GameEngine Instance = null;
+
+	void Awake()
+	{
+		if (Instance != null) 
+		{
+			Debug.LogError("A second GameState has been created!");
+		}
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		this._fencePieces = new List<Fence>();
+		this._enemies = new List<Enemy>();
+		this._player = new Player();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void AddSheep() 
+	{
+		
+	}
+	void RemoveSheep()
+	{
+	
+	}
+}
