d539c3b0dfdc82f51015e0cdf9d59c27861610b4
Charles H Andrus <charles.h.andrus@gmail.com>
2014-10-04 00:33:20 -0500

    Added code for GameEngine.

--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -7,6 +7,9 @@ public class GameEngine : MonoBehaviour {
 	private List<Fence> _fencePieces;
 	private List<Enemy> _enemies;
 	private Player _player;
+	public int TotalSheep;
+	public int MaxSheepOnBoardAtOnce;
+	private int _sheepAdded;
 
 	public static GameEngine Instance = null;
 
@@ -17,6 +20,7 @@ public class GameEngine : MonoBehaviour {
 			Debug.LogError("A second GameState has been created!");
 		}
 		Instance = this;
+		
 	}
 
 	// Use this for initialization
@@ -25,19 +29,31 @@ public class GameEngine : MonoBehaviour {
 		this._fencePieces = new List<Fence>();
 		this._enemies = new List<Enemy>();
 		this._player = new Player();
+		this._sheepAdded = 0;
+		this.TotalSheep = 10;
+		this.MaxSheepOnBoardAtOnce = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () 
-	{
-	
+	{	
+		AddEnemy();
+		System.Threading.Thread.Sleep(1000);
+		foreach (Enemy e in _enemies)
+		{
+			e.Move();
+		}
 	}
-	void AddSheep() 
+	void AddEnemy() 
+	{
+		if (_enemies.Count < MaxSheepOnBoardAtOnce && _sheepAdded < TotalSheep)
+		{
+			_enemies.Add(new Enemy());
+			_sheepAdded++;
+		}
+	}
+	void RemoveEnemy()
 	{
 		
-	}
-	void RemoveSheep()
-	{
-	
 	}
 }
