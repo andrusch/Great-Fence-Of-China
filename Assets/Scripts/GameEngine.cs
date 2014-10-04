b496c0f81ee6ce1caf4dbf922159e6c425a175bf
Charles H Andrus <charles.andrus@orangeinfusion.com>
2014-10-04 12:35:23 -0500

    New code for piece and game engine.

--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -4,15 +4,20 @@ using System.Collections.Generic;
 
 public class GameEngine : MonoBehaviour {
 	private Object[,] _grid;
+	
 	private List<Fence> _fencePieces;
 	private List<Enemy> _enemies;
 	private Player _player;
-	public int TotalSheep;
+	public int TotalSheepInLevel;
+	public int TotalSheepKilledInAllLevels;
 	public int MaxSheepOnBoardAtOnce;
 	public GameObject SheepPrefab;
 	private int _sheepAdded;
-	
+	public int Level;
+	public int BoardHeight;
+	public int BoardWidth;
 	public static GameEngine Instance = null;
+	private int[] _enemyCountPerRow;
 
 	void Awake()
 	{
@@ -21,20 +26,22 @@ public class GameEngine : MonoBehaviour {
 			Debug.LogError("A second GameState has been created!");
 		}
 		Instance = this;
-		
 	}
-
 	// Use this for initialization
 	void Start () 
 	{
+		this.Level = 1;
+		this.BoardHeight = 5;
+		this.BoardWidth = 13;
 		this._fencePieces = new List<Fence>();
 		this._enemies = new List<Enemy>();
 		this._player = new Player();
 		this._sheepAdded = 0;
-		this.TotalSheep = 10;
+		this.TotalSheepInLevel = 10 * this.Level;
 		this.MaxSheepOnBoardAtOnce = 1;
+		this._grid = new Object[this.BoardHeight, this.BoardWidth];
+		this._enemyCountPerRow = new int[this.BoardHeight];
 	}
-	
 	// Update is called once per frame
 	void Update () 
 	{	
@@ -45,15 +52,78 @@ public class GameEngine : MonoBehaviour {
 			e.Move();
 		}
 	}
+	
+	void OnGUI()
+	{		
+		if (this == null)
+			Debug.LogError("CANNOT FIND MY PARENT");
+		else
+		{
+			Vector2 targetPos;
+			Rect cameraRect = Camera.main.pixelRect;
+			
+			if (this.IsLevelOver())
+			{
+				if (this.DidIWin())
+					GUI.Box(new Rect(Screen.width /2 - 100, 10, 170, 20),  "You Win! " + this.GetScore().ToString());
+				else 
+					GUI.Box(new Rect(Screen.width /2 - 100, 10, 170, 20),  "You Lose! " + this.GetScore().ToString());
+			}
+			else
+			{
+				GUI.Box(new Rect(Screen.width /2 - 100, 10, 170, 20),  "Your Score Is: " + this.GetScore().ToString());
+			}
+		}
+	}
 	void AddEnemy() 
 	{
-		if (_enemies.Count < MaxSheepOnBoardAtOnce && _sheepAdded < TotalSheep)
+		if (_enemies.Count < MaxSheepOnBoardAtOnce && _sheepAdded < TotalSheepInLevel)
 		{
-			_enemies.Add(new Enemy());
+			Enemy e = new Enemy();
+			e.x = 0;
+			e.y = GenerateYForEnemy();
+			_enemies.Add(e);
+			_enemyCountPerRow[e.y]++;
 			_sheepAdded++;
 		}
 	}
+	void int GenerateYForEnemy()
+	{
+		int row = 0;
+		int minCount = _enemyCountPerRow[0];
+		if (minCount == 0)
+			return minCount;
+		for (int i =1; i<this.BoardHeight; i++)
+		{
+			if (_enemyCountPerRow[i] == 0)
+			{
+				row = i;
+				minCount = _enemyCountPerRow[i];
+				break;
+			}				
+			if (minCount > _enemyCountPerRow[i])		
+			{
+				row = i;
+				minCount = _enemyCountPerRow[i];				
+			}
+		}
+		return row;
+	}
+	
 	void RemoveEnemy()
+	{
+		
+	
+	}
+	bool IsLevelOver()
+	{
+		return _enemies.Count == 0 && TotalSheepInLevel == _sheepAdded;
+	}
+	bool DidIWin()
+	{
+		return IsLevelOver() && this._player.Health > 0;
+	}
+	bool TryMove(Object piece, int x, int y)
 	{
 		
 	}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,14 @@ public class Player : MonoBehaviour {
 	public int x;
 	public int y;
 	public int Health;
+	public float xMargin = 1f;		// Distance in the x axis the  can move before the camera follows.
+	public float yMargin = 1f;		// Distance in the y axis the focus can move before the camera follows.
+	public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
+	public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
+	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
+	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
+	private bool m_isHoriAxisInUse = false;
+	private bool m_isVertAxisInUse = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,13 +24,78 @@ public class Player : MonoBehaviour {
 	void Update () {
 	
 	}
-	public void Move()
+	
+	void FixedUpdate() 
 	{
 
+				float h = Input.GetAxisRaw("Horizontal");
+				float v = Input.GetAxisRaw("Vertical");
+				float tempX = this.StartX;
+				float tempY = this.StartY;
+				if( h != 0.0f)
+				{
+					if(m_isHoriAxisInUse == false)
+					{
+						if  (((tempX+h) < maxXAndY.x) && ((tempX+h) > minXAndY.x))
+							tempX += h;
+						m_isHoriAxisInUse = true;
+					}
+				}
+				if( h == 0.0f)
+				{
+					m_isHoriAxisInUse = false;
+				} 
+				if( v != 0.0f)
+				{
+					if(m_isVertAxisInUse == false)
+					{
+						if  (((tempY+v) < maxXAndY.y) && ((tempY+v) > minXAndY.y))
+							tempY += v;
+						m_isVertAxisInUse = true;
+					}
+				}
+				if( v == 0.0f)
+				{
+					m_isVertAxisInUse = false;
+				} 
+				Vector2 tempVector = new Vector2(tempX,tempY);
+				//Debug.LogError("testing...");
+				
+				if (tempX != this.StartX || tempY != this.StartY)
+				{
+					if (this.TryMove((int)tempX, (int)tempY))
+					{
+						gameObject.transform.position = tempVector;
+					}
+				}
+	}
+	
+	void OnGUI()
+	{		
+		if (this == null)
+			Debug.LogError("CANNOT FIND MY PARENT");
+		else if (GameState.instance.IsPlayerTurn)
+		{
+			Vector2 targetPos;
+			targetPos = Camera.main.WorldToScreenPoint (new Vector3(this.StartX, this.StartY, transform.position.z));
+			GUI.Box(new Rect(targetPos.x-40, Screen.height- targetPos.y - 50, 75, 20), "Your Move");
+		}
+		else
+		{
+			Vector2 targetPos;
+			targetPos = Camera.main.WorldToScreenPoint (new Vector3(this.StartX, this.StartY, transform.position.z));
+			GUI.Box(new Rect(targetPos.x-60, Screen.height- targetPos.y - 50, 115, 20), "Computer's Move");
+		}
+		
+	}
+	
+	public void Move(int newStartX, int newStartY)
+	{
+		GameEngine.Instance.MoveObject (this, newStartX, newStartY);
 	}
 	public void TakeDamage()
 	{
 
-		}
+	}
 
 }
