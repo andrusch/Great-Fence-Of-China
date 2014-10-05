d2634ec044e0b99f9e3770f3ad8ae571aba9573d
Stephen Hmiel <ephesossh@gmail.com>
2014-10-04 21:02:49 -0500

    Sprite cleanup, hopefully initial setup of explosions

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour 
+{
+	public Texture[] frames;
+	public int framesPerSecond = 10;
+	public float speed = .8f;
+	int counter = 0;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();
+//		anim.StopPlayback();
+		anim.speed = speed;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		counter++;
+		if(counter > 50)
+			Destroy(gameObject);
+//		int index = (counter * framesPerSecond ) % frames.Length;
+//		renderer.material.mainTexture = frames[index];
+//		counter++;
+
+	}
+}
